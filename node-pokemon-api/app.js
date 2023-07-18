dadebf49cda15e88165ec411183574e3205f89d4
GAEL AMBEU <ambeugael128@gmail.com>
2023-07-18 04:13:11 +0200

    short modif
    use paramater id for identify all objet easy

--- node-pokemon-api/app.js
+++ node-pokemon-api/app.js
@@ -3,5 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello, Express  !'))
+app.get('/', (req, res) => res.send('I am Express !'))
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Vous avez demandé le pokémon n°${id}`)
+})
+
+
+
 app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`)) 
