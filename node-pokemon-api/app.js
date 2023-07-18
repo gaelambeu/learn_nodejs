3e8a2eec8bd5c981a8d5380585766576e77d912e
GAEL AMBEU <ambeugael128@gmail.com>
2023-07-18 03:40:33 +0200

    use nodemon
    for refresh page

--- node-pokemon-api/app.js
+++ node-pokemon-api/app.js
@@ -3,5 +3,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello, Express !'))
-app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`))
+app.get('/', (req, res) => res.send('Hello, Express  !'))
+app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`)) 
