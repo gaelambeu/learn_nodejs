1dbbaba7813e489911d5e7a87689b61e2f97132f
GAEL AMBEU <ambeugael128@gmail.com>
2023-07-21 22:38:02 +0200

    short modif

--- node-pokemon-api/app.js
+++ node-pokemon-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
@@ -6,8 +7,9 @@ const port = 3000
 app.get('/', (req, res) => res.send('I am Express !'))
 
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = req.params.id
-    res.send(`Vous avez demandé le pokémon n°${id}`)
+    const id = parseInt (req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}`)
 })
 
 
---
+++ node-pokemon-api/mock-pokemon.js
@@ -0,0 +1,22 @@
+const pokemons = [
+    {
+        id: 1,
+        name: "Bulbizarre",
+        types:["Plante"],
+        created: new Date()
+    },
+
+    {
+        id: 2,
+        name: "Body",
+        types:["Show"],
+        created: new Date()
+    },
+
+    {
+        id: 3,
+        name: "Back",
+        types:["Ground"],
+        created: new Date()
+    },
+]
