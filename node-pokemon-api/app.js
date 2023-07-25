463ff948ee7b9f944d104e10ba660416e1ae13ce
GAEL AMBEU <ambeugael128@gmail.com>
2023-07-25 02:43:38 +0200

    showing all pokemons

--- node-pokemon-api/app.js
+++ node-pokemon-api/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { success } = require('./helper.js')
-let {pokemons} = require('./mock-pokemon')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('I am Express !'))
+
+// Le nouveau point de terminaison, affichan,t le nombre total de pokémons:
+app.get('/api/pokemons', (req, res) => {
+    const message = 'La liste des pokémons a bien été trouvé'
+    res.json(success(message, pokemons))
+})
 
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id)
@@ -14,9 +20,6 @@ app.get('/api/pokemons/:id', (req, res) => {
     res.json(success(message, pokemon))
 })
 
-// Le nouveau point de terminaison, affichan,t le nombre total de pokémons:
-app.get('/api/pokemons', (req, res) => {
-    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex pour le moment`)
-})
+
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`)) 
